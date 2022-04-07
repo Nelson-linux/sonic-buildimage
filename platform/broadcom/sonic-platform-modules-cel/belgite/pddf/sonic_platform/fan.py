4052e81f03d620779a0f2b9793a772acf9474fd0
JerseyYang <jersyang@celestica.com>
2022-04-07 17:30:04 +0800

    [Author]:Jersey Yang [Jira ID]:N/A [Description]:fix the watchdog and fan led issue

--- platform/broadcom/sonic-platform-modules-cel/belgite/pddf/sonic_platform/fan.py
+++ platform/broadcom/sonic-platform-modules-cel/belgite/pddf/sonic_platform/fan.py
@@ -3,6 +3,12 @@ try:
     import os
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
+# ------------------------------------------------------------------
+# HISTORY:
+#    5/1/2022 (A.D.)
+#    add function:set_status_led,
+#    Solve the problem that when a fan is pulled out, the Fan LED on the front panel is still green Issue-#11525
+# ------------------------------------------------------------------
 FAN_DIRECTION_FILE_PATH = "/var/fan_direction"
 
 
@@ -12,7 +18,6 @@ class Fan(PddfFan):
     def __init__(self, tray_idx, fan_idx=0, pddf_data=None, pddf_plugin_data=None, is_psu_fan=False, psu_index=0):
         # idx is 0-based
         PddfFan.__init__(self, tray_idx, fan_idx, pddf_data, pddf_plugin_data, is_psu_fan, psu_index)
-
 
     def get_speed_tolerance(self):
         """
@@ -25,11 +30,10 @@ class Fan(PddfFan):
         # Fix the speed vairance to 10 percent. If it changes based on platforms, overwrite
         # this value in derived pddf fan class
         return 20
-    
-    
+
     def get_presence(self):
-        #Overwirte the PDDF Common since the FANs on Belgite are all Fixed and present
-        return True 
+        # Overwirte the PDDF Common since the FANs on Belgite are all Fixed and present
+        return True
 
     def get_direction(self):
         """
@@ -55,13 +59,11 @@ class Fan(PddfFan):
                 if fan_direction.strip() == "FB":
                     direction = "EXHAUST"
         return direction
-    
 
     def get_status(self):
         speed = self.get_speed_rpm()
         status = True if (speed != 0) else False
         return status
-
 
     def get_target_speed(self):
         """
@@ -76,7 +78,7 @@ class Fan(PddfFan):
             # Target speed not usually supported for PSU fans
             target_speed = "N/A"
         else:
-            idx = (self.fantray_index-1)*1 + self.fan_index
+            idx = (self.fantray_index - 1) * 1 + self.fan_index
             attr = "fan" + str(idx) + "_pwm"
             pwm_path = "/sys/devices/pci0000:00/0000:00:12.0/i2c-0/i2c-2/2-0066/" + attr
             pwm = 0
@@ -84,7 +86,26 @@ class Fan(PddfFan):
                 pwm = f.read()
 
             percentage = int(pwm.strip())
-            speed_percentage = int(round(percentage/255*100))
+            speed_percentage = int(round(percentage / 255 * 100))
             target_speed = speed_percentage
 
         return target_speed
+
+    def set_status_led(self, color):
+        color_dict = {"green": "STATUS_LED_COLOR_GREEN",
+                      "red": "STATUS_LED_COLOR_AMBER"}
+        color = color_dict.get(color, "off")
+        index = str(self.fantray_index - 1)
+        led_device_name = "FANTRAY{}".format(self.fantray_index) + "_LED"
+
+        result, msg = self.pddf_obj.is_supported_sysled_state(led_device_name, color)
+        if result is False:
+            print(msg)
+            return False
+        device_name = self.pddf_obj.data[led_device_name]['dev_info']['device_name']
+        self.pddf_obj.create_attr('device_name', device_name, self.pddf_obj.get_led_path())
+        self.pddf_obj.create_attr('index', index, self.pddf_obj.get_led_path())
+        self.pddf_obj.create_attr('color', color, self.pddf_obj.get_led_cur_state_path())
+
+        self.pddf_obj.create_attr('dev_ops', 'set_status', self.pddf_obj.get_led_path())
+        return True
